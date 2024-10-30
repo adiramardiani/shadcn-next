d08a8f64225819f26843fa98cf85eede9d06bd13
adiramardiani <adira.mardiani@gmail.com>
2024-10-30 07:58:54 +0700

    Add dummy content to check bug h/v scroll on sidebar layout

--- app/(public)/template.tsx
+++ app/(public)/template.tsx
@@ -12,7 +12,20 @@ import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar';
 
 export default function DashboardTemplate({ children }: { children: ReactNode }) {
   return (
-    <SidebarInset>
+    <SidebarInset
+    // using this style just work well,the scroll is gone
+    // but sticky nav header / breadcrumb is not work well
+
+    // style={
+    //   {
+    //     width: 'calc(100vw-(var(--sidebar-width)))',
+    //     overflow: 'hidden'
+    //   } as React.CSSProperties
+    // }
+
+    // using this has has no effect
+    // className="max-w-full"
+    >
       <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
         <div className="flex items-center gap-2 px-4">
           <SidebarTrigger className="-ml-1" />
